6798b93f95ec9937a0f9063083dd7a088940d451
Jonathan Chiu <quinnqiu0823@icloud.com>
2025-04-26 03:12:51 +0800

    Update footer.js

--- scripts/footer.js
+++ scripts/footer.js
@@ -16,5 +16,58 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>. */
 
 (function() {
-    
+    let baseUri = 'https://quinn0823.github.io/universal/'
+    baseUri = document.baseURI;
+    const domain = baseUri.split('/')[2];
+    const githubUserName = domain.split('.')[0];
+    const githubRepoName = baseUri.split('/')[3];
+    const githubRepoUrl = `https://github.com/${githubUserName}/${githubRepoName}`;
+    console.log(baseUri, domain, githubUserName, githubRepoName, githubRepoUrl);
+
+    const LICENSE_MAP = {
+        'GPL-3.0': 'GNU General Public License v3.0'
+    };
+
+    const metaContent = {
+        'author': 'Jonathan Chiu',
+        'license': null,
+        'year': new Date().getFullYear().toString(),
+        'updated-date': null,
+    };   
+    Object.keys(metaContent).forEach(key => {
+        metaContent[key] = document.querySelector(`meta[name="${key}"]`)?.content || metaContent[key];
+    });
+    console.log(metaContent);
+
+    let footerHtmlArray = [];
+
+    // Updated Date
+    if (metaContent['updated-date']) {
+        const updatedDate = new Date(metaContent['updated-date']).toLocaleDateString('en-US', { 
+            month: 'long', 
+            day: 'numeric', 
+            year: 'numeric'
+        });
+        const updatedDateHtml = `Updated Date: <time datetime="${metaContent['updated-date']}">${updatedDate}</time>`
+        footerHtmlArray.push(updatedDateHtml);
+    }
+
+    // Copyright & License
+    if (metaContent['license']) {
+        const copyrightHtml = `Copyright © ${metaContent['year']} ${metaContent['author']}`;
+        const license = LICENSE_MAP[metaContent['license']];
+        const licenseHtml = `Licensed under the <a href="${githubRepoUrl}/LICENSE" target="_blank">${license}</a>.`
+        footerHtmlArray.push(copyrightHtml, licenseHtml);
+    } else {
+        const copyrightHtml = `Copyright © ${metaContent['year']} ${metaContent['author']} All rights reserved.`;
+        footerHtmlArray.push(copyrightHtml);
+    }
+
+    footerHtmlArray = footerHtmlArray.map(item => `<p>${item}</p>`);
+    const footerHtml = `<hr />${footerHtmlArray.join('')}`;
+
+    console.log(footerHtmlArray, footerHtml);
+
+    const footer = document.getElementById('universal-footer');
+    footer.innerHTML = footerHtml;
 })();
