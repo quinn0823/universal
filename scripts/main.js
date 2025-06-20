64436d71faa56864572ea4f296754fdf9f70ed26
Jonathan Chiu <quinnqiu0823@icloud.com>
2025-06-20 21:08:46 +0800

    Update clearValueById()

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+/* Universal
+The MIT License (MIT)
+Copyright (c) 2025 Jonathan Chiu */
+
+function clearValueById(elementId) {
+    const elementElement = document.getElementById(elementId);
+    elementElement.value = "";
+}
