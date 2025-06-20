47108a78a9906ed7d0e637aef2d9db737dff71c1
Jonathan Chiu <quinnqiu0823@icloud.com>
2025-06-20 20:46:28 +0800

    header, footer -> layout

--- scripts/layout.js
+++ scripts/layout.js
@@ -56,6 +56,37 @@ function getLocalization(key) {
 }
 
 (function () {
+    let headerHtmlArray = [];
+
+    // Title
+    let title = document.title.split(' | ')[0];
+    let titleElement = document.getElementById('universal-title');
+    if (titleElement && titleElement.innerHTML) {
+        title = titleElement.innerHTML;
+    }
+    // console.log(title);
+    const titleHtml = `<h1 id="universal-title">${title}</h1>`;
+    headerHtmlArray.push(titleHtml);
+
+
+    const headerMetaContent = {
+        'description': null
+    };
+    Object.keys(headerMetaContent).forEach(key => {
+        headerMetaContent[key] = document.querySelector(`meta[name="${key}"]`)?.content || metaContent[key];
+    });
+    // console.log(metaContent);
+
+    // Description
+    const description = metaContent['description'];
+    const descriptionHtml = `<p id="universal-description">${description}</p>`;
+    headerHtmlArray.push(descriptionHtml);
+
+    headerHtml = headerHtmlArray.join('');
+
+    const headerElement = document.getElementById('universal-header');
+    headerElement.innerHTML = headerHtml;
+
     let baseUri = document.baseURI;
     if (!baseUri.includes('github.io')) {
         baseUri = 'https://quinn0823.github.io/universal/';
